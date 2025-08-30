3e1ee799e5a4ee9d74de0a91e4d45a424d7ed07f
Jhonatan Konopp <jhonnkonopp@gmail.com>
2025-08-30 02:18:51 -0300

    feat: created status policy

---
+++ backend/src/sku/policies/sku-status.policy.ts
@@ -0,0 +1,21 @@
+import { SkuStatus, AllowedTransitionStatus } from '../entities/sku.entity';
+
+export class SkuStatusPolicy {
+  static canChangeStatus(current: SkuStatus, next: SkuStatus): boolean {
+    const allowedTransitions: Record<
+      (typeof AllowedTransitionStatus)[number],
+      SkuStatus[]
+    > = {
+      [SkuStatus.PRE_REGISTER]: [SkuStatus.CANCEL],
+      [SkuStatus.COMPLETED_REGISTER]: [SkuStatus.ACTIVE, SkuStatus.CANCEL],
+      [SkuStatus.ACTIVE]: [SkuStatus.INACTIVE],
+      [SkuStatus.INACTIVE]: [SkuStatus.ACTIVE, SkuStatus.PRE_REGISTER],
+    };
+
+    if (current === SkuStatus.CANCEL) {
+      throw new Error('Cannot change CANCELED status.');
+    }
+
+    return allowedTransitions[current].includes(next);
+  }
+}
