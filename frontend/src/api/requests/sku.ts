6954cbf8aea47e661f21e96b48b0b87bb8dd2c47
Jhonatan Konopp <jhonnkonopp@gmail.com>
2025-08-31 11:36:25 -0300

    refactor: prettier format

--- frontend/src/api/requests/sku.ts
+++ frontend/src/api/requests/sku.ts
@@ -1,33 +1,36 @@
-import type { AxiosResponse } from "axios";
-import { api } from "../instance";
-import type { RegisterSkuPayload, Sku, UpdateSkuInfoPayload, UpdateSkuStatusPayload } from "../types/sku";
+import type { AxiosResponse } from 'axios';
+import { api } from '../instance';
+import type {
+  RegisterSkuPayload,
+  Sku,
+  UpdateSkuInfoPayload,
+  UpdateSkuStatusPayload,
+} from '../types/sku';
 
 export async function fetchSkus(): Promise<AxiosResponse<Sku[]>> {
   return api.get('/sku');
 }
 
 export async function registerSku(
-  payload: RegisterSkuPayload
+  payload: RegisterSkuPayload,
 ): Promise<AxiosResponse<Sku>> {
   return api.post('/sku/register', payload);
 }
 
-export async function getSku(
-  id: string
-): Promise<AxiosResponse<Sku | null>> {
+export async function getSku(id: string): Promise<AxiosResponse<Sku | null>> {
   return api.get(`/sku/${id}`);
 }
 
 export async function updateSkuInfo(
   id: string,
-  payload: UpdateSkuInfoPayload
+  payload: UpdateSkuInfoPayload,
 ): Promise<AxiosResponse<Sku>> {
   return api.patch(`/sku/${id}`, payload);
 }
 
 export async function updateSkuStatus(
   id: string,
-  payload: UpdateSkuStatusPayload
+  payload: UpdateSkuStatusPayload,
 ): Promise<AxiosResponse<Sku>> {
   return api.patch(`/sku/${id}/status`, payload);
 }
