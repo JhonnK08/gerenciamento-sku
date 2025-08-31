266170920924ac7b60c5c904b3f72e033c390464
Jhonatan Konopp <jhonnkonopp@gmail.com>
2025-08-31 02:53:25 -0300

    fix: database module

--- backend/src/sku/sku.module.ts
+++ backend/src/sku/sku.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SkuService } from './sku.service';
 import { SkuController } from './sku.controller';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [SkuController],
   providers: [SkuService],
 })
