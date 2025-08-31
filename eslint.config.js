8c06adc3eaa5426f63b73f4947d3fba431186e25
Jhonatan Konopp <jhonnkonopp@gmail.com>
2025-08-31 11:36:04 -0300

    chore: fix eslint config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,30 @@
-// eslint-config/index.js
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
+import { dirname } from 'path';
 import tseslint from 'typescript-eslint';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
   {
+    ignores: ['eslint.config.js']
+  },
+  {
     languageOptions: {
       globals: {
         ...globals.es2021,
       },
       parserOptions: {
-        projectService: true,
         tsconfigRootDir: __dirname,
+        projectService: {
+          allowDefaultProject: ['*.js', '*.mjs', '*.cjs'],
+        },
       },
     },
   },
--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,26 +3,32 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { globalIgnores } from 'eslint/config';
 import globals from 'globals';
+import { dirname } from 'path';
 import tseslint from 'typescript-eslint';
-import baseConfig from '../eslint.config';
+import { fileURLToPath } from 'url';
+import baseConfig from '../eslint.config.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'eslint.config.js']),
+  ...baseConfig,
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.vite,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      baseConfig,
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
         project: './tsconfig.json',
         tsconfigRootDir: __dirname,
+        projectService: {
+          allowDefaultProject: ['*.js', '*.mjs', '*.cjs'],
+        },
       },
     },
   },
