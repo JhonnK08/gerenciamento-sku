44d2410065b22908be960ea7c4c355cd27c978ce
Jhonatan Konopp <jhonnkonopp@gmail.com>
2025-09-01 13:34:48 -0300

    fix: cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+		methods: ['GET', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+		origin: [
+			'http://localhost:5173',
+		]
+	});
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
